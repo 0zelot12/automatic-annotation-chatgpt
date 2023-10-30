6b8e2a70b73d33d94d8ecc203abb06f20fea0ae5
alauritz <lauritz.andreas@gmail.com>
2023-10-30 23:12:55 +0100

    Test api

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from langchain.llms import OpenAI
+
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+from langchain.prompts import PromptTemplate
 
 llm = OpenAI()
 chat_model = ChatOpenAI()
+
+template = "You are a process manager. Generate a textual description for the following process: {process}"
+prompt = PromptTemplate.from_template(template)
+
+messages = [HumanMessage(content=prompt.format(process="Publish a video game"))]
+llm_answer = llm.predict_messages(messages)
+
+print(llm_answer)
