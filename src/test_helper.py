55378668395113e66b0010f4f5d98da726c34b31
alauritz <lauritz.andreas@gmail.com>
2024-02-26 21:02:29 +0100

    Tests

---
+++ src/test_helper.py
@@ -0,0 +1,44 @@
+import unittest
+
+from entity import Entity
+from entity_type import EntityType
+
+from helper import parse_entities
+
+
+class TestHelperMethods(unittest.TestCase):
+    # TODO: Empty case, Invalid Case
+    def test_parse_entities(self):
+        self.assertEqual(
+            [
+                Entity(EntityType.ACTOR, 0, ["The", "MPON"]),
+                Entity(EntityType.ACTIVITY, 2, ["sents"]),
+                Entity(EntityType.ACTIVITY_DATA, 3, ["the", "dismissal"]),
+                Entity(EntityType.ACTOR, 6, ["the", "MPOO"]),
+            ],
+            parse_entities(
+                [
+                    "<ACTOR>",
+                    "The",
+                    "MPON",
+                    "</ACTOR>",
+                    "<ACTIVITY>",
+                    "sents",
+                    "</ACTIVITY>",
+                    "<ACTIVITY_DATA>",
+                    "the",
+                    "dismissal",
+                    "</ACTIVITY_DATA>",
+                    "to",
+                    "<ACTOR>",
+                    "the",
+                    "MPOO",
+                    "</ACTOR>",
+                    ".",
+                ]
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
