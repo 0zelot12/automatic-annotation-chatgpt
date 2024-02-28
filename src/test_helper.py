4395e67aec9d37bcda3d6d73e4642c767531f0ff
alauritz <lauritz.andreas@gmail.com>
2024-02-28 20:18:05 +0100

    Add another test

--- src/test_helper.py
+++ src/test_helper.py
@@ -3,8 +3,9 @@ from annotation_metrics import AnnotationMetrics
 
 from entity import Entity
 from entity_type import EntityType
+from entity_tag import EntityTag
 
-from helper import parse_entities, calculate_metrics
+from helper import parse_entities, calculate_metrics, convert_to_template_example
 
 
 class TestHelperMethods(unittest.TestCase):
@@ -92,6 +93,104 @@ class TestHelperMethods(unittest.TestCase):
             ],
         )
         self.assertEqual(metrics, expected_metrics)
+
+    def test_convert_to_template_example(self):
+        tokens = [
+            "The",
+            "manufacturer",
+            "sends",
+            "the",
+            "invoice",
+            "to",
+            "the",
+            "customer",
+            ".",
+            "If",
+            "the",
+            "customer",
+            "pays",
+            "within",
+            "two",
+            "weeks",
+            "the",
+            "order",
+            "is",
+            "marked",
+            "as",
+            "completed",
+            ".",
+        ]
+
+        ner_tags = [
+            EntityTag.B_ACTOR,
+            EntityTag.I_ACTOR,
+            EntityTag.B_ACTIVITY,
+            EntityTag.B_ACTIVITY_DATA,
+            EntityTag.I_ACTIVITY_DATA,
+            EntityTag.NO_ENTITY,
+            EntityTag.B_ACTOR,
+            EntityTag.I_ACTOR,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.B_ACTOR,
+            EntityTag.I_ACTOR,
+            EntityTag.B_ACTIVITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.B_ACTIVITY,
+            EntityTag.I_ACTIVITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+            EntityTag.NO_ENTITY,
+        ]
+
+        expected_example = [
+            "<ACTOR>",
+            "The",
+            "manufacturer",
+            "</ACTOR>",
+            "<ACTIVITY>",
+            "sends",
+            "</ACTIVITY>",
+            "<ACTIVITY_DATA>",
+            "the",
+            "invoice",
+            "</ACTIVITY_DATA>",
+            "to",
+            "<ACTOR>",
+            "the",
+            "customer",
+            "</ACTOR>",
+            ".",
+            "If",
+            "<ACTOR>",
+            "the",
+            "customer",
+            "</ACTOR>",
+            "<ACTIVITY>",
+            "pays",
+            "</ACTIVITY>",
+            "within",
+            "two",
+            "weeks",
+            "the",
+            "order",
+            "<ACTIVITY>",
+            "is",
+            "marked",
+            "</ACTIVITY>",
+            "as",
+            "completed",
+            ".",
+        ]
+
+        self.assertEqual(
+            convert_to_template_example(tokens, ner_tags),
+            expected_example,
+        )
 
 
 if __name__ == "__main__":
