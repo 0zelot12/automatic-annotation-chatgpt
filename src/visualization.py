d164cbc178048eb208bcad3e6ecd50dc141bd99d
alauritz <lauritz.andreas@gmail.com>
2024-02-29 21:54:47 +0100

    Remove unused code

--- src/visualization.py
+++ src/visualization.py
@@ -2,23 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def convert_to_html(tokens_with_tags: list[str]) -> str:
-    content = ""
-    for token in tokens_with_tags:
-        if token.startswith("<actor>"):
-            token = token.replace("<actor>", "")
-            content += f"<span class='actor'>{token}</span> "
-        elif token.startswith("<activity>"):
-            token = token.replace("<activity>", "")
-            content += f"<span class='activity'>{token}</span> "
-        elif token.startswith("<activity_data>"):
-            token = token.replace("<activity_data>", "")
-            content += f"<span class='activity-data'>{token}</span> "
-        else:
-            content += f"{token} "
-    return content
-
-
 def generate_simple_bar_chart(values, categories, xLabel, yLabel, title):
     """
     Generate a simple bar chart using matplotlib.
