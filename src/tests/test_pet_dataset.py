82681632b91c9737a835b331183d20d4229dda3e
alauritz <lauritz.andreas@gmail.com>
2024-04-07 20:41:28 +0200

    Test

--- src/tests/test_pet_dataset.py
+++ src/tests/test_pet_dataset.py
@@ -1,4 +1,35 @@
-from pet.pet_dataset import PetDataset
+from pet.pet_dataset import PetDataset, get_linear_index
+
+test_sentence_ids = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    2,
+    2,
+    2,
+    2,
+    2,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    4,
+    4,
+    4,
+    4,
+]
 
 
 def test_singleton_instance():
@@ -29,3 +60,8 @@ def test_get_document_by_name():
         and document.ner_tags
         and document.tokens
     )
+
+
+def test_get_linear_index():
+    linear_index = get_linear_index(sentence_ids=test_sentence_ids, sid=3, wid=2)
+    assert linear_index == 17
