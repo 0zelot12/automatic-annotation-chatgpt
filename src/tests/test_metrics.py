e6adecbff0895225c0d1334a053c8dadb0fefa72
alauritz <andreas.lauritz@xavo.com>
2024-05-08 00:18:38 +0200

    Tests

--- src/tests/test_metrics.py
+++ src/tests/test_metrics.py
@@ -1,4 +1,4 @@
-from utils.helper import get_recall, get_precision
+from utils.helper import get_recall, get_precision, get_f1_score
 
 
 def test_recall():
@@ -22,16 +22,24 @@ def test_recall_one():
 def test_precision():
     true_positives = 10
     total = 100
-    assert get_recall(true_positives, total) == 0.1
+    assert get_precision(true_positives, total) == 0.1
 
 
 def test_precision_zero():
     true_positives = 0
     total = 0
-    assert get_recall(true_positives, total) == 0.0
+    assert get_precision(true_positives, total) == 0.0
 
 
 def test_precision_one():
     true_positives = 10
     postives_total = 10
-    assert get_recall(true_positives, postives_total) == 1.0
+    assert get_precision(true_positives, postives_total) == 1.0
+
+
+def test_f1_score():
+    assert get_f1_score(0.3, 0.6) == 0.4
+
+
+def test_f1_score_zero():
+    assert get_f1_score(1.0, 0) == 0.0
