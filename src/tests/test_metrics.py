64bc31a0b16fb3df1642ebcb6fa612f2bf573175
alauritz <andreas.lauritz@xavo.com>
2024-05-15 21:02:59 +0200

    WIP

--- src/tests/test_metrics.py
+++ src/tests/test_metrics.py
@@ -2,39 +2,39 @@ from utils.helper import get_recall, get_precision, get_f1_score
 
 
 def test_recall():
-    true_positives = 10
-    postives_total = 100
-    assert get_recall(true_positives, postives_total) == 0.1
+    ok = 10
+    gold = 100
+    assert get_recall(ok, gold) == 0.1
 
 
 def test_recall_zero():
-    true_positives = 0
-    postives_total = 0
-    assert get_recall(true_positives, postives_total) == 0.0
+    ok = 0
+    gold = 0
+    assert get_recall(ok, gold) == 0.0
 
 
 def test_recall_one():
-    true_positives = 10
-    postives_total = 10
-    assert get_recall(true_positives, postives_total) == 1.0
+    ok = 10
+    gold = 10
+    assert get_recall(ok, gold) == 1.0
 
 
 def test_precision():
-    true_positives = 10
-    total = 100
-    assert get_precision(true_positives, total) == 0.1
+    ok = 10
+    pred = 100
+    assert get_precision(ok, pred) == 0.1
 
 
 def test_precision_zero():
-    true_positives = 0
-    total = 0
-    assert get_precision(true_positives, total) == 0.0
+    ok = 0
+    pred = 0
+    assert get_precision(ok, pred) == 0.0
 
 
 def test_precision_one():
-    true_positives = 10
-    postives_total = 10
-    assert get_precision(true_positives, postives_total) == 1.0
+    ok = 10
+    pred = 10
+    assert get_precision(ok, pred) == 1.0
 
 
 def test_f1_score():
